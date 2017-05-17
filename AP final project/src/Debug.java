4df25ce5761b128926c4cb2de9f0ab1fab4c5fd7
max13692 <maxkassel@10.49.4.152>
2017-05-17 18:01:21 -0500

    Completed the Debug class and added comments in for each method.
    Also added in a get time method for both of the debug methods and
    finished the getVarType and the getPath methods. 
    
    Added some code to the Tester class, this is completely unnecessary and
    should be deleted when needed.

--- AP final project/src/Debug.java
+++ AP final project/src/Debug.java
@@ -1,27 +1,79 @@
+import java.text.SimpleDateFormat;
 
+/*
+ * The Debug class is for debugging the code during creation. 
+ * This class will be used instead of System.out.println 
+ * because it can be changed for our needs during the creation
+ * of this project. 
+ * ---------------------
+ * Written by: Max Kassel
+ * ---------------------
+ */
 public class Debug {
-	public static void print(String text){
+	// The format for the current time:
+	static SimpleDateFormat tf = new SimpleDateFormat("HH:mm:ss.SSS");
+
+	/*
+	 * print takes in a String and then outputs that String to the console with
+	 * no next line.
+	 */
+	public static void print(String text) {
 		System.out.print(text);
 	}
-	public static void println(String text){
+
+	/*
+	 * println takes a String as an input then outputs that String to the
+	 * console with a next line.
+	 */
+	public static void println(String text) {
 		System.out.println(text);
 	}
-	public static void debug(String variableName, Object variable){
-		System.out.println(getPath() + ": '" + getVarType(variable) + " "+ variableName + " = " + variable + "'");
+
+	/*
+	 * debug takes in a String and an Object. The String will be used as the
+	 * variable name and the object will be used as the value of the variable.
+	 * The time, path and variable will be printed out on the line with a next
+	 * line underneath it.
+	 * 
+	 */
+	public static void debug(String variableName, Object variable) {
+		System.out.println(getTime() + " " + getPath() + ": '" + getVarType(variable) + " " + variableName + " = "
+				+ variable + "'");
 	}
-	public static void debug(String text){
-		System.out.println(getPath() + ": '" + text + "'");
+
+	/*
+	 * debug takes in a String and outputs the time, path and the String with a
+	 * next line. This is used for debug statements
+	 */
+	public static void debug(String text) {
+		System.out.println(getTime() + " " + getPath() + ": '" + text + "'");
 	}
-	private static  String getVarType(Object obj){
-		if(obj instanceof Integer){
-			return "int";
-		}
-		else{
-			return obj.getClass().toString();
-		}
+
+	/*
+	 * getVarType takes in an object then returns the type of variable as a
+	 * String.
+	 * 
+	 */
+	private static String getVarType(Object obj) {
+		return obj.getClass().getSimpleName();
 	}
-	public static String getPath(){
+
+	/*
+	 * getPath returns the location of the call. This is only used in debug
+	 * statements to find the location of the code. This might NOT work with
+	 * other methods because it only works 2 method calls behind.
+	 */
+	private static String getPath() {
 		String s = Thread.currentThread().getStackTrace()[3].toString();
-		return  s.substring(0, s.indexOf('('))  + " [Line:" + s.substring(s.indexOf(":")+1,s.indexOf(')',s.indexOf(":"))) + "]";
+		return s.substring(0, s.indexOf('(')) + " [Line:"
+				+ s.substring(s.indexOf(":") + 1, s.indexOf(')', s.indexOf(":"))) + "]";
+	}
+
+	/*
+	 * getTime returns the current time with the format of tf. To change format
+	 * change tf.
+	 */
+	private static String getTime() {
+		return "(" + tf.format(System.currentTimeMillis()) + ")";
 	}
 }
--- AP final project/src/Tester.java
+++ AP final project/src/Tester.java
@@ -1,6 +1,7 @@
-import javax.swing.JOptionPane;
+
 public class Tester {
 	public static void main(String args[]){
 		Debug.debug("args", args.length);
+		Debug.debug("this is just some text");
 	}
 }
