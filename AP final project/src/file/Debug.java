29304e4acb28005443936b8f757a134210a0cb9d
max13692 <maxkassel@10.49.6.142>
2017-05-18 14:19:48 -0500

    Small edits

--- AP final project/src/file/Debug.java
+++ AP final project/src/file/Debug.java
@@ -1,4 +1,5 @@
 package file;
+
 import java.text.SimpleDateFormat;
 
 /*
--- AP final project/src/file/Tester.java
+++ AP final project/src/file/Tester.java
@@ -4,9 +4,8 @@ import javax.swing.JOptionPane;
 
 public class Tester {
 	public static void main(String args[]){
-				PopUp.buttonMessage("Welcome to the game","The Game", new String[] {"Start", "Help", "Close", "extra"});
-				String name = PopUp.textInput("whats your name?");
-				PopUp.dropDownMessage("Hey boiz", "whats up", new String[] {"NO", "YEs", "coolio", "maybe"});
+		int a = 0;
+				Debug.debug("adfdf", a);
 			}
 			
 	
