53948476563c94fd98668651db2baea4adaa3bf0
16693 <16693@HS27917.verona.k12.wi.us>
2017-05-19 11:47:04 -0500

    Time class mostly done. Person class skeleton created

---
+++ AP final project/src/file/Player.java
@@ -0,0 +1,11 @@
+package file;
+
+public class Player {
+	String playerName;
+	double health, hunger, sanity, thirst;
+	Time time;
+	public Player(String name){
+		playerName = name;
+		
+	}
+}
---
+++ AP final project/src/file/Time.java
@@ -0,0 +1,40 @@
+package file;
+
+public class Time {
+	final int END_TIME = 365;
+	int days, hours, minutes;
+	public Time(){
+		days = 1;
+		hours = 6;
+		minutes = 30;
+	}
+	public String getTime(){
+		if(hours <= 12)
+			return hours + ":" + minutes + " am";
+		return hours-12 + ":" + minutes + " pm";
+	}
+	public int getDay(){
+		return days;
+	}
+	
+	public void addTime(int hours){
+		this.hours += hours;
+	}
+	private void updateTime(){
+		while(minutes > 60){
+			minutes -= 60;
+			hours++;
+		}
+		while(hours > 24){
+			hours -= 24;
+			days++;
+		}	
+	if(days >= END_TIME){
+		//end of game
+	}
+	}
+	public static void addTime(int hours, int minutes){
+		
+		
+	}
+}
