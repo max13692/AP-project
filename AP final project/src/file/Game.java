154f5f0c60b37089051a3350f25c80556259d404
kyllan16693 <fencer4933@gmail.com>
2017-05-24 13:28:10 -0500

    Started game menu and created shop and actions methods in game method. Max: finish methods?

--- AP final project/src/file/Game.java
+++ AP final project/src/file/Game.java
@@ -11,7 +11,28 @@ public class Game {
 	}
 
 	private static void gameMenu() {
-		
+		int game = PopUp.buttonMessage("What do you want to do?", new String[] {"Actions", "Open Shop", "View Stats","Quit"});
+		Debug.debug("game", game);
+		switch(game) {
+		case 0:
+			Debug.debug("Closing program...");
+			System.exit(0);
+			break;
+		case 1:
+			Debug.debug("Player has opened userMenu...");
+			userMenu(1);
+			break;
+		case 2:
+			Debug.debug("Player has opened the shop...");
+			shop();
+			break;
+		case 3:
+			Debug.debug("Player opened action menu...");
+			actions();
+			break;
+		default:
+			Debug.debug("Invalid selection...");
+		}
 	}
 
 	private static void userMenu(int menuNum) {
@@ -38,5 +59,15 @@ public class Game {
 			Debug.error("menuNum", menuNum);
 			Debug.error("This should not have this number.");
 		}
+	}
+	
+	private static void shop() {
+		
+		
+		
+	}
+	
+	private static void actions() {
+		
 	}
 }
