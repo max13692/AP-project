8c5109bdb429969289b5d8b4a9204d3c26cc8815
kyllan16693 <fencer4933@gmail.com>
2017-06-02 13:44:51 -0500

    Sleep method added and sleeping works

--- AP final project/src/file/Game.java
+++ AP final project/src/file/Game.java
@@ -13,8 +13,8 @@ public class Game {
 	}
 
 	private static void gameMenu() {
-		int game = PopUp.buttonMessage("What do you want to do?",
-				new String[] { "Quit", "View Stats", "Open Shop", "Actions" });
+		int game = PopUp.buttonMessage("What do you want to do?", new String[] {
+				"Quit", "View Stats", "Open Shop", "Actions" });
 		Debug.debug("game", game);
 		switch (game) {
 		case -1:
@@ -58,12 +58,15 @@ public class Game {
 		switch (menuNum) {
 		case 1:
 			int stats = PopUp.buttonMessage(
-					"--------------" + player + "--------------" + "\nExp: " + player.getExp() + "/"
-							+ player.getRequiredExp() + "\nHealth:  " + getBar((int) player.getHealth()) + "\nHunger: "
-							+ getBar((int) player.getHunger()) + "\nThirst:   " + getBar((int) player.getHunger())
-							+ "\nSanity:   " + getBar((int) player.getSanity()) + "\nWeapon: " + player.getDefualtItem()
-							+ "\nGold: " + player.getGold() + "g",
-					"stats", new String[] { "Change wepeon", "Go back" });
+					"--------------" + player + "--------------" + "\nExp: "
+							+ player.getExp() + "/" + player.getRequiredExp()
+							+ "\nHealth:  " + getBar((int) player.getHealth())
+							+ "\nHunger: " + getBar((int) player.getHunger())
+							+ "\nThirst:   " + getBar((int) player.getHunger())
+							+ "\nSanity:   " + getBar((int) player.getSanity())
+							+ "\nWeapon: " + player.getDefualtItem()
+							+ "\nGold: " + player.getGold() + "g", "stats",
+					new String[] { "Change wepeon", "Go back" });
 			Debug.debug("stats", stats);
 			switch (stats) {
 			case -1:
@@ -90,7 +93,8 @@ public class Game {
 			String list[] = new String[player.getItems().size()];
 			for (int i = 0; i < player.getItems().size(); i++)
 				list[i] = player.getItem(i).toString();
-			String item = PopUp.dropDownMessage("Choose a weapon to use as your main weapon.", list);
+			String item = PopUp.dropDownMessage(
+					"Choose a weapon to use as your main weapon.", list);
 			for (int i = 0; i < list.length; i++)
 				if (list[i].equals(item)) {
 					player.setDefaultItem(i);
@@ -136,8 +140,11 @@ public class Game {
 
 	private static void shop() {
 		String matrix[][] = Save.getMatrixFromFile("Data/shop.txt");
-		int shop = PopUp.buttonMessage("Welcome to the shopping district. Where would you like to shop?",
-				new String[] { "Go Back", "Witch Hut", "Bar", "Grocery Store", "Weapon Shop" });
+		int shop = PopUp
+				.buttonMessage(
+						"Welcome to the shopping district. Where would you like to shop?",
+						new String[] { "Go Back", "Witch Hut", "Bar",
+								"Grocery Store", "Weapon Shop" });
 		Debug.debug("shop", shop);
 		String[] shopItems = null;
 		String selection = null;
@@ -148,7 +155,9 @@ public class Game {
 		switch (shop) {
 		case 4:
 			shopItems = getShopData(matrix, "I", 4);
-			selection = PopUp.dropDownMessage("Welcome to the weapons shop. What can I get for you?", shopItems);
+			selection = PopUp.dropDownMessage(
+					"Welcome to the weapons shop. What can I get for you?",
+					shopItems);
 			Debug.debug("selection", selection);
 			if (selection == null)
 				shop();
@@ -156,7 +165,12 @@ public class Game {
 			Debug.debug("selection", selection);
 			Debug.debug("Checking if player has item.");
 			for (int i = 0; i < playerWeapons.size(); i++)
-				if (playerWeapons.get(i).toString().substring(0, playerWeapons.get(i).toString().indexOf("(") - 1)
+				if (playerWeapons
+						.get(i)
+						.toString()
+						.substring(
+								0,
+								playerWeapons.get(i).toString().indexOf("(") - 1)
 						.equals(selection)) {
 					hasWeapon = true;
 					break;
@@ -170,14 +184,17 @@ public class Game {
 				PopUp.textInput("You already have this weapon.");
 			else {
 				player.subtractGold(cost);
-				player.addItem(new Item(matrix[location][1], Double.parseDouble(matrix[location][2]),
-						Double.parseDouble(matrix[location][3])));
+				player.addItem(new Item(matrix[location][1], Double
+						.parseDouble(matrix[location][2]), Double
+						.parseDouble(matrix[location][3])));
 				PopUp.textMessage("Weapon Bought");
 			}
 			break;
 		case 3:
 			shopItems = getShopData(matrix, "F", 3);
-			selection = PopUp.dropDownMessage("Welcome to the Grocery Store! What would you like?", shopItems);
+			selection = PopUp.dropDownMessage(
+					"Welcome to the Grocery Store! What would you like?",
+					shopItems);
 			Debug.debug("selection", selection);
 			if (selection == null)
 				shop();
@@ -197,7 +214,10 @@ public class Game {
 			break;
 		case 2:
 			shopItems = getShopData(matrix, "D", 3);
-			selection = PopUp.dropDownMessage("Welcome to the bar! What can I serve ya today?", shopItems);
+			selection = PopUp
+					.dropDownMessage(
+							"Welcome to the bar! What can I serve ya today?",
+							shopItems);
 			Debug.debug("selection", selection);
 			if (selection == null)
 				shop();
@@ -217,7 +237,9 @@ public class Game {
 			break;
 		case 1:
 			shopItems = getShopData(matrix, "P", 6);
-			selection = PopUp.dropDownMessage("Welcome to my hut! What could I brew for you today?", shopItems);
+			selection = PopUp.dropDownMessage(
+					"Welcome to my hut! What could I brew for you today?",
+					shopItems);
 			Debug.debug("selection", selection);
 			if (selection == null)
 				shop();
@@ -274,22 +296,25 @@ public class Game {
 				if (random >= 5 && random < 30) {
 					double gold = Math.random() + 1;
 					Debug.debug("gold", gold);
-					PopUp.textMessage("You found " + (int) (gold * random) + " gold.");
+					PopUp.textMessage("You found " + (int) (gold * random)
+							+ " gold.");
 					player.addGold(((int) (gold * random)));
 				}
 				if (random >= 12 && random <= 36) {
 					double exp = Math.random() + 1;
 					Debug.debug("exp", exp);
-					PopUp.textMessage("You also get " + (int) ((exp * random) / 2) + " experience.");
+					PopUp.textMessage("You also get "
+							+ (int) ((exp * random) / 2) + " experience.");
 					player.addExp((int) ((exp * random) / 2));
 				}
 				if (random <= 5 && random < 18) {
-					PopUp.textMessage("You got stung by a bee -" + random / 2 + " health");
+					PopUp.textMessage("You got stung by a bee -" + random / 2
+							+ " health");
 					player.subtractHealth(random / 2);
 				} else if (random >= 18 && random < 24) {
 					fight(1);
 				} else if (random >= 24 && random < 30) {
-fight(2);
+					fight(2);
 					// find random food or drink
 				} else if (random >= 30 && random <= 36) {
 					fight(3);
@@ -300,6 +325,21 @@ fight(2);
 			gameMenu();
 			break;
 		case 2:
+			Debug.debug("Going to sleep");
+			int sleepHours = (int) ((Math.random() * player.getTime()
+					.getHours()) / 3);
+			int ran2 = (int) (Math.random() * 3);
+			if (sleepHours >= 7) {
+				PopUp.textMessage("You slept really well.");
+				player.addSanity(sleepHours * ran2);
+			} else if (sleepHours < 7 && sleepHours >= 5) {
+				PopUp.textMessage("You slept ok.");
+				player.addSanity(sleepHours * ran2);
+			} else {
+				PopUp.textMessage("You didn't sleep well.");
+				player.addSanity((int) (sleepHours * ran2) / 2);
+
+			}
 
 			break;
 		case 3:
@@ -308,20 +348,29 @@ fight(2);
 
 		}
 	}
-	private static void fight(int difficulty){
+
+	private static void fight(int difficulty) {
 		String names[][] = Save.getMatrixFromFile("Data/enemies.txt");
-		Enemy enemy = new Enemy(names[(int)(Math.random()*names.length)][0],(int) (Math.random() * player.getLevel() * difficulty * 2)+1, (int) (Math.random() * player.getLevel() * difficulty)+1, (int) (Math.random() * player.getLevel() * difficulty)+1, (int) (Math.random() * player.getLevel()) + difficulty);
+		Enemy enemy = new Enemy(names[(int) (Math.random() * names.length)][0],
+				(int) (Math.random() * player.getLevel() * difficulty * 2) + 1,
+				(int) (Math.random() * player.getLevel() * difficulty) + 1,
+				(int) (Math.random() * player.getLevel() * difficulty) + 1,
+				(int) (Math.random() * player.getLevel()) + difficulty);
 		Debug.debug("enemy", enemy);
-		PopUp.textMessage("You have incountered: " + enemy + "\nHealth: " + getBar((int)enemy.getHealth()) + "\nAttack: " + enemy.getAttack() + " Defence: " + enemy.getDefense());
-		while(!player.isDead() && !enemy.isDead()){
-			PopUp.textMessage(enemy.takeDamamge(player.getDefualtItem().getAttack()));
-			if(enemy.isDead())
+		PopUp.textMessage("You have incountered: " + enemy + "\nHealth: "
+				+ getBar((int) enemy.getHealth()) + "\nAttack: "
+				+ enemy.getAttack() + " Defence: " + enemy.getDefense());
+		while (!player.isDead() && !enemy.isDead()) {
+			PopUp.textMessage(enemy.takeDamamge(player.getDefualtItem()
+					.getAttack()));
+			if (enemy.isDead())
 				break;
 			PopUp.textMessage(player.takeDamage(enemy.getAttack()));
-			Debug.debug("enemyHealth",enemy.getHealth());
-			Debug.debug("playerHealth",player.getHealth());
+			Debug.debug("enemyHealth", enemy.getHealth());
+			Debug.debug("playerHealth", player.getHealth());
 		}
 	}
+
 	private static void quitGame() {
 		Debug.debug("Closing game...");
 		Debug.debug("Saving game...");
